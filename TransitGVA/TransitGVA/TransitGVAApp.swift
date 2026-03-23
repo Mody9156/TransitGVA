c01da71c1957105e750cf86b48a891f5d0af4867
KEÏTA <tenin@MacBook-Air-de-KEITA.local>
2026-03-23 20:52:56 +0100

    new project

---
+++ TransitGVA/TransitGVA/TransitGVAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransitGVAApp.swift
+//  TransitGVA
+//
+//  Created by KEÏTA on 23/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct TransitGVAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

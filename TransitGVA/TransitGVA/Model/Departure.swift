0107830321a3cda1f1c20df61a6a5ea06e223d0f
KEÏTA <tenin@MacBook-Air-de-KEITA.local>
2026-03-24 15:55:31 +0100

    new struct

--- TransitGVA/TransitGVA/Model/Departure.swift
+++ TransitGVA/TransitGVA/Model/Departure.swift
@@ -7,20 +7,6 @@
 
 import Foundation
 
-struct TPGDeparture: Codable {
-    let lineCode: String
-    let destinationName: String
-    let waitingTime: String       // ex: "5 min" ou "Departure"
-    let timestamp: String?        // heure réelle ISO8601
-
-    enum CodingKeys: String, CodingKey {
-        case lineCode        = "line_code"
-        case destinationName = "destination_name"
-        case waitingTime     = "waiting_time"
-        case timestamp       = "timestamp"
-    }
-}
-
 // Modèle propre utilisé dans l'UI
 struct Departure: Identifiable {
     let id = UUID()
--- TransitGVA/TransitGVA/Model/TPGDeparture.swift
+++ TransitGVA/TransitGVA/Model/TPGDeparture.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct TPGDeparture: Codable {
+    let lineCode: String
+    let destinationName: String
+    let waitingTime: String       // ex: "5 min" ou "Departure"
+    let timestamp: String?        // heure réelle ISO8601
+
+    enum CodingKeys: String, CodingKey {
+        case lineCode        = "line_code"
+        case destinationName = "destination_name"
+        case waitingTime     = "waiting_time"
+        case timestamp       = "timestamp"
+    }
+}

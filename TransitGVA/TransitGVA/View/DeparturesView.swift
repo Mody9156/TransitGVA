cc3237268aadcf4b789aee3420e3c24f0abe4591
KEÏTA <tenin@MacBook-Air-de-KEITA.local>
2026-03-24 16:05:55 +0100

    new view

---
+++ TransitGVA/TransitGVA/View/DeparturesView.swift
@@ -0,0 +1,18 @@
+//
+//  DeparturesView.swift
+//  TransitGVA
+//
+//  Created by KEÏTA on 24/03/2026.
+//
+
+import SwiftUI
+
+struct DeparturesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeparturesView()
+}
